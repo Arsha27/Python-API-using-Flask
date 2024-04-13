c8ed203b2c45cd63553d25d154b4a1426cde417c
Arsha27 <110929229+Arsha27@users.noreply.github.com>
2024-04-13 15:12:10 +0530

    get request

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ from flask import Flask, request,jsonify
 app = Flask(__name__)   #to start a flask application
 
 #create a route 
-@app.route("/")    #setting the default route 
+@app.route("/get-user/<user_id>")    #http method/path parameter dynamic path you can pass to a url
 
-def home():
-    return "HOME"
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name" : "Arsha Renjith",
+        "email": "arsha@example.com"
+    }
+
+    #query parameter- addition variable to pass
+    
+    extra= request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data),200
 
 if __name__ == "__main__":    #run our flask server
     app.run(debug=True)
